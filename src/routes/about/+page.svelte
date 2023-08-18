0e68eba204d17db6ec8a0b26cc63441cc49aa697
Matt Ferraro <mferraro89@gmail.com>
2023-08-17 17:08:44 -0700

    Added three.js

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { createScene } from '$lib/scene.js';
+
+	let el;
+	if (browser) {
+		onMount(() => {
+			console.log('el 3', el);
+			createScene(el);
+		});
+	}
+</script>
+
+<p>Canvas:</p>
+
+<canvas bind:this={el} />
+
+<p>Hello</p>
+
+<style>
+	canvas {
+		width: 80%;
+		height: 80%;
+		border: 1px solid black;
+	}
+</style>
